08e871274efd70c022ca966bc9d02ffea8b18f80
Mario Henrique <mariohemriquejbe2016@gmail.com>
2025-09-30 14:46:45 -0300

    Update app.js

--- Protocol-HTTP/app.js
+++ Protocol-HTTP/app.js
@@ -1,4 +1,6 @@
 var http = require('http');
-http.createServer(function(req,res){res.end("Hello World. Welcome to te jungle")}).listen(8081);
+http.createServer(function(req,res){
+    res.end("Hello World. Welcome to te jungle")
+}).listen(8081);
 
 console.log("o servidor está rodando!")
